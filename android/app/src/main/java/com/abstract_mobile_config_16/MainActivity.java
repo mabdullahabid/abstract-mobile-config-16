4986104fa201a4a55aa025822754593e0873b27d
cb-abd-dev <cb@abd.dev>
2022-09-08 20:43:38 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/abstract_mobile_config_16/MainActivity.java
@@ -0,0 +1,15 @@
+package com.abstract_mobile_config_16;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "abstract_mobile_config_16";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "abstract_mobile_config_16",
+  url: "https://abstract_mobile_config_16.botics.co",
+  api: "https://abstract_mobile_config_16.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
